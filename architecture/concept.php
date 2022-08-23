634721d9a3b39afd6625cf5ec9d37ce4cabbab98
introzorn <introzorn@yandex.ru>
2022-08-23 13:26:31 +0700

    concept

--- architecture/concept.php
+++ architecture/concept.php
@@ -1,9 +1,37 @@
 <?php
+
+interface UserConfigLoader{
+    public function getCFG(String $key);
+}
+
+Class UserConfigFILE implements UserConfigLoader{
+    public function getCFG(String $key){}
+}
+Class UserConfigDB implements UserConfigLoader{
+    public function getCFG(String $key){}
+}
+Class UserConfigREDIS implements UserConfigLoader{
+    public function getCFG(String $key){}
+}
+Class UserConfigCLOUD implements UserConfigLoader{
+    public function getCFG(String $key){}
+}
+Class UserConfigECT implements UserConfigLoader{
+    public function getCFG(String $key){}
+}
+
+
 class Concept {
     private $client;
 
     public function __construct() {
         $this->client = new \GuzzleHttp\Client();
+    }
+
+    public function getSecretKey():String{
+        $driver=USER_CONFIG_DRIVER;
+        $UserConfig = new $driver;
+        return $UserConfig->getCFG('SecretKey');
     }
 
     public function getUserData() {
